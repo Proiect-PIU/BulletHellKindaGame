e2c8080891ee54779880c65a0694a3e2b815f737
Francisc-Gabriel <francisc-gabriel.virlan@student.tuiasi.ro>
2024-12-04 20:42:20 +0200

    acuma impuscaturile se detecteaza

--- Creators/Updater/Updater.cpp
+++ Creators/Updater/Updater.cpp
@@ -6,7 +6,7 @@
 
 void Updater::update(Level &level, Canvas &canvas, CollisionMatrix &matrix, float deltaTime, GLFWwindow &window) {
     for (auto& entity : level.waves[level.currentWave]->entities) {
-        entity->update(canvas, deltaTime, window);
+        entity->update(canvas, matrix, deltaTime, window);
         entity->self->shape->update(entity->self->element->getModelMatrix());
         matrix.AddElement(*entity->self->shape, entity->self->state);
         canvas.addElement(std::move(std::make_unique<Element>(*entity->self->element)));
